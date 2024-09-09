574e56e78600b2fd3ba8287d18270fe231234d96
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-09-09 22:32:04 +0100

    created the files and added the route to the global route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { HomeController } from "./home/home.controller";
 
 import AuthRouter from './auth/auth.routes';
+import CustomerRouter from './customer/customer.routes';
 
 const router = Router();
 
@@ -11,6 +12,9 @@ router.get("/", home.index);
 
 // Authentication Module
 router.use('/auth', AuthRouter);
+
+// Customer Module
+router.use('/customer', CustomerRouter);
 
 
 export default router;
