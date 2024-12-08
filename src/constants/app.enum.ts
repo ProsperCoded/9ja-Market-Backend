7e7e3486a2d2146bb13227d1bd2a2961d4e91177
Obycodez55 <obikoyaadebayo55@gmail.com>
2024-12-08 10:59:00 +0100

    Update BASE_URL and CLIENT_URL to use config service for dynamic configuration

--- src/constants/app.enum.ts
+++ src/constants/app.enum.ts
@@ -1,6 +1,8 @@
+import { configService } from "../utils/config/config.service";
+
 export enum AppEnum {
     PORT = 3000,
     PREFIX = '/api/v1',
-    BASE_URL = "http://localhost:3000/api/v1",
-    CLIENT_URL = "http://localhost:3001"
+    BASE_URL = configService.get("BASE_URL") || "http://localhost:3000/api/v1" as unknown as number,
+    CLIENT_URL = configService.get("CLIENT_URL") || "http://localhost:5173" as unknown as number,
 }
