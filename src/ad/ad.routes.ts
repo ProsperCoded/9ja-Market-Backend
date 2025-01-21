85feefce850353e7ed1135d5e7947d0d6699ffad
Obycodez55 <obikoyaadebayo55@gmail.com>
2025-01-21 12:50:36 +0100

    Implement ad routes for activating free ads and initializing/verifying payments

--- src/ad/ad.routes.ts
+++ src/ad/ad.routes.ts
@@ -1,11 +1,43 @@
 import { Router } from "express";
+import { AdService } from "./ad.service";
+import { AdRepository } from "../repositories/ad.repository";
+import { QuickTellerPaymentRepository } from "../repositories/payment.repository";
+import { ProductRepository } from "../repositories/product.repository";
+import { TransactionRepository } from "../repositories/transaction.repository";
+import { WinstonLogger } from "../utils/logger/winston.logger";
+import { AdController } from "./ad.controller";
+import { Validator } from "../utils/middlewares/validator.middleware";
+import { MerchantAuthGaurd } from "../utils/middlewares/guards/merchant.auth.guard";
+import { MerchantRepository } from "../repositories/merchant.repository";
+import { JWTService } from "../utils/jwt/jwt.service";
+import { InitializeAdPaymentDto } from "./dtos/initialize-ad-payment.dto";
 
 
 
 const router = Router();
 
-router.get("/", (req, res) => {});
+const merchantRepository = new MerchantRepository();
+const jwtService = new JWTService()
+const adRepository = new AdRepository();
+const paymentService = new QuickTellerPaymentRepository();
+const productRepository = new ProductRepository();
+const transactionRepository = new TransactionRepository();
+const logger = new WinstonLogger("AdService");
+const adService = new AdService(adRepository, paymentService, productRepository, transactionRepository, logger);
+const adController = new AdController(adService);
 
+const validator = new Validator();
+const merchantAuthGaurd = new MerchantAuthGaurd(merchantRepository, logger, jwtService);
+
+
+// Activate Free Ad
+router.post("/free/:productId", merchantAuthGaurd.authorise({strict: true}), adController.activateFreeAd);
+
+// Initialize Ad Payment
+router.post("/initialize/:level/:productId", validator.single(InitializeAdPaymentDto, "params") ,merchantAuthGaurd.authorise({strict: true}), adController.initializeAdPayment);
+
+// Verify Ad Payment
+router.post("/verify/:reference", merchantAuthGaurd.authorise({strict: true}), adController.verifyAdPayment);
 
 
 export default router;
