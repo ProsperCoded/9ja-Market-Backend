d9516e7f2c9d2d23be0f950a24434b71fe5e7b00
ProsperCoded <enweremproper@gmail.com>
2025-03-25 10:31:30 +0100

    feat: enhance NodemailerProvider to improve email sending functionality
    - ✨ Added support for sending emails with sender name and address.
    - 🔧 Refactored code for better readability and maintainability.

--- src/utils/email/providers/nodemailer.provider.ts
+++ src/utils/email/providers/nodemailer.provider.ts
@@ -8,61 +8,78 @@ import { configService } from "../../config/config.service";
 import SMTPPool from "nodemailer/lib/smtp-pool";
 import { EmailPaths, EmailSubjects } from "../../../constants/email.enum";
 
-class OAuth2Client extends google.auth.OAuth2 { }
+class OAuth2Client extends google.auth.OAuth2 {}
 
 export default class NodemailerProvider implements IEmailService {
-    private readonly OAuth2Client: OAuth2Client;
-    private readonly transporter: Transporter;
-    private logger: ILogger;
+  private readonly OAuth2Client: OAuth2Client;
+  private readonly transporter: Transporter;
+  private logger: ILogger;
 
-    constructor(logger: ILogger) {
-        this.logger = logger;
-        this.OAuth2Client = new google.auth.OAuth2(
-            configService.get<string>("GMAIL_CLIENT_ID"),
-            configService.get<string>("GMAIL_CLIENT_SECRET"),
-            "https://developers.google.com/oauthplayground" // or your redirect URI
-          );
-          this.OAuth2Client.setCredentials({
-            refresh_token: configService.get<string>("GMAIL_REFRESH_TOKEN"),
-          });
-        this.transporter = createTransport({
-            service: 'gmail',
-            auth: {
-            type: 'OAuth2',
-            user: configService.get<string>("COMPANY_EMAIL"),
-            clientId: configService.get<string>("GMAIL_CLIENT_ID"),
-            clientSecret: configService.get<string>("GMAIL_CLIENT_SECRET"),
-            refreshToken: configService.get<string>("GMAIL_REFRESH_TOKEN"),
+  constructor(logger: ILogger) {
+    this.logger = logger;
+    this.OAuth2Client = new google.auth.OAuth2(
+      configService.get<string>("GMAIL_CLIENT_ID"),
+      configService.get<string>("GMAIL_CLIENT_SECRET"),
+      "https://developers.google.com/oauthplayground" // or your redirect URI
+    );
+    this.OAuth2Client.setCredentials({
+      refresh_token: configService.get<string>("GMAIL_REFRESH_TOKEN"),
+    });
+    this.transporter = createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: configService.get<string>("COMPANY_EMAIL"),
+        clientId: configService.get<string>("GMAIL_CLIENT_ID"),
+        clientSecret: configService.get<string>("GMAIL_CLIENT_SECRET"),
+        refreshToken: configService.get<string>("GMAIL_REFRESH_TOKEN"),
+      },
+    } as unknown as SMTPPool);
+  }
+
+  public sendMail({
+    to,
+    subject,
+    options,
+  }: {
+    to: string;
+    subject: EmailSubjects;
+    options: { template: EmailPaths; data: { [key: string]: any } };
+  }): Promise<boolean> {
+    (this.transporter as any).accessToken =
+      this.OAuth2Client.getAccessToken() as any;
+
+    return new Promise<boolean>((resolve, reject) => {
+      const template = path.resolve("view/emails", options.template);
+      ejs.renderFile(template, options.data, (error, html) => {
+        if (error) {
+          this.logger.error("Error rendering email template", error);
+          reject(error);
+          return;
+        }
+        this.transporter.sendMail(
+          {
+            from: {
+              name: configService.get<string>("COMPANY_NAME"),
+              address: configService.get<string>("COMPANY_EMAIL"),
             },
-        } as unknown as SMTPPool);
-    }
-
-    public sendMail({ to, subject, options }: { to: string; subject: EmailSubjects; options: { template: EmailPaths; data: { [key: string]: any; }; }; }): Promise<boolean> {
-        (this.transporter as any).accessToken = this.OAuth2Client.getAccessToken() as any;
-
-        return new Promise<boolean>((resolve, reject) => {
-            const template = path.resolve("view/emails", options.template);
-            ejs.renderFile(template, options.data, (error, html) => {
-                if (error) {
-                    this.logger.error("Error rendering email template", error);
-                    reject(error);
-                    return;
-                }
-                this.transporter.sendMail({
-                    from: configService.get<string>("COMPANY_EMAIL"),
-                    to,
-                    subject,
-                    html
-                }, (err, info) => {
-                    if (err) {
-                        this.logger.error("Error sending email", err);
-                        reject(err);
-                        return;
-                    }
-                    this.logger.info("Email sent successfully");
-                    resolve(true);
-                });
-            });
-        });
-    }
+            to,
+            subject,
+            html,
+          },
+          //   port: 587/465,
+          //   secure: true // true for 465, false for other ports
+          (err, info) => {
+            if (err) {
+              this.logger.error("Error sending email", err);
+              reject(err);
+              return;
+            }
+            this.logger.info("Email sent successfully");
+            resolve(true);
+          }
+        );
+      });
+    });
+  }
 }
